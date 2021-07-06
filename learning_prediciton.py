b58234a82a978591a1b91bad83556e3a21eda1c9
Justus Hübotter <justus.huebotter@donders.ru.nl>
2021-07-06 18:02:23 +0200

    first predictive model seems to work :)

--- learning_prediciton.py
+++ learning_prediciton.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import nengo
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+mpl.use("Qt5Agg")
 import os
 from tqdm import tqdm
 from pathlib import Path
@@ -11,14 +13,14 @@ from models.predictive_model import make_model
 experiment_name = "test1"
 data_dir = "data/Train/"
 results_dir = "results/"
-load_weights = "" # results/test1/2021-07-06_16.32.41.441352/weights_latest.npy"
-bound = 0.29            # if the cart ever leaves these bounds, the data is ignored
-epochs = 2              # number or epochs for training
+load_weights = ""
+bound = 0.19            # if the cart ever leaves these bounds, the data is ignored
+epochs = 10             # number or epochs for training
 samp_freq = 50          # cartpole data is recorded at ~50Hz
 dt = 0.001              # nengo time step
 learning_rate = 5e-5    # lr
 t_delay = 0.02          # how far to predict the future (initial guess)
-n = 100                 # number of neurons representing each dimension
+neurons_per_dim = 100   # number of neurons representing each dimension
 seed = 4                # to get reproducible neuron properties across runs
 
 # crating a unique folder to save the weights in
@@ -50,13 +52,15 @@ if load_weights:
     weights = np.load(load_weights)
 else:
     print("initializing weights as zeros")
-    weights = np.zeros((4, n * 5))
+    weights = np.zeros((4, neurons_per_dim * 5))
 
 # train the model
 all_errors = []
+all_baseline_errors = []
 for e in range(epochs):
     print("\nstarting epoch", e+1)
     epoch_mean_errors = []
+    epoch_baseline_errors = []
     with tqdm(total=len(training_data)) as t:
         for i, df in enumerate(training_data):
             action_df = df[["time", "Q"]]
@@ -77,7 +81,7 @@ for e in range(epochs):
                 state_df,
                 weights=weights,
                 seed=seed,
-                n=n,
+                n=neurons_per_dim,
                 samp_freq=samp_freq,
                 t_delay=t_delay,
                 learning_rate=learning_rate
@@ -85,18 +89,38 @@ for e in range(epochs):
             sim = nengo.Simulator(model, progress_bar=False)
             sim.run(t_max)
             weights = sim.data[recordings[P_WEIGHTS]][-1]
-            mean_error = np.mean(np.abs(sim.data[recordings[P_E]]))
+            p_e = sim.data[recordings[P_E]]
+            p_z = sim.data[recordings[P_Z]]
+            p_z_pred = sim.data[recordings[P_Z_PRED]]
+            p_s = sim.data[recordings[P_S]]
+
+            mean_error = np.mean(np.abs(p_e))
+            baseline_error = np.mean(np.abs(p_z_pred - p_s))
             epoch_mean_errors.append(mean_error)
+            epoch_baseline_errors.append(baseline_error)
             all_errors.append(mean_error)
+            all_baseline_errors.append(baseline_error)
             t.set_postfix(loss="{:05.4f}".format(mean_error))
             t.update()
+
+    """
+    plt.figure()
+    plt.plot(range(len(p_z)), p_z)
+    plt.plot(range(len(p_s)), p_s)
+    plt.plot(range(len(p_z_pred)), p_z_pred)
+    plt.show()
+    plt.close()
+    """
+
 
     print(f"\nepoch mean loss: {np.mean(epoch_mean_errors)}")
     np.save(Path(run_dir, "weights_latest"), weights)
 
     plt.plot(range(len(all_errors)), all_errors)
+    plt.plot(range(len(all_baseline_errors)), all_baseline_errors)
     plt.xlabel("Example")
-    plt.ylabel("Prediction error")
+    plt.ylabel("Error")
+    plt.legend(["Prediction", "Baseline"])
     plt.savefig(Path(run_dir, "error_curve.svg"))
-    plt.show()
+    # plt.show()
     plt.close()
