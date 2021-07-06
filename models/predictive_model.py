6be4f34c521d0262f4e1e5357041aa39766fd28a
Nikhil Garg <f2016597@goa.bits-pilani.ac.in>
2021-07-07 01:05:02 +0530

    A first attemt for LMUs
    Does not work at the moment. Have to rectify the connections to be done correctly. Not sure how to connect the input from one dimension to one dimension of LDN and subsequently from LDN to the hidden layer

--- models/predictive_model.py
+++ models/predictive_model.py
@@ -187,3 +187,166 @@ def make_model(action_df, state_df, weights=None, seed=42, n=100, samp_freq=50,
         p_weights = nengo.Probe(conn, 'weights', sample_every=0.1)
 
     return model, [p_a, p_s, p_z, p_z_pred, p_e, p_weights]
+
+'''
+
+LMU Nengo models and implementation
+
+To use in network: ldn = nengo.Node(LDN(theta=theta, q=q))
+
+'''
+
+import matplotlib.pyplot as plt
+import nengo
+import numpy as np
+import scipy.linalg
+from scipy.special import legendre
+
+class LDN(nengo.Process):
+    def __init__(self, theta, q, size_in=1):
+        self.q = q              # number of internal state dimensions per input
+        self.theta = theta      # size of time window (in seconds)
+        self.size_in = size_in  # number of inputs
+
+        # Do Aaron's math to generate the matrices A and B so that
+        #  dx/dt = Ax + Bu will convert u into a legendre representation over a window theta
+        #  https://github.com/arvoelke/nengolib/blob/master/nengolib/synapses/analog.py#L536
+        A = np.zeros((q, q))
+        B = np.zeros((q, 1))
+        for i in range(q):
+            B[i] = (-1.)**i * (2*i+1)
+            for j in range(q):
+                A[i,j] = (2*i+1)*(-1 if i<j else (-1.)**(i-j+1)) 
+        self.A = A / theta
+        self.B = B / theta        
+        
+        super().__init__(default_size_in=size_in, default_size_out=q*size_in)
+
+    def make_step(self, shape_in, shape_out, dt, rng, state=None):
+        state = np.zeros((self.q, self.size_in))
+
+        # Handle the fact that we're discretizing the time step
+        #  https://en.wikipedia.org/wiki/Discretization#Discretization_of_linear_state_space_models
+        Ad = scipy.linalg.expm(self.A*dt)
+        Bd = np.dot(np.dot(np.linalg.inv(self.A), (Ad-np.eye(self.q))), self.B)
+
+        # this code will be called every timestep
+        def step_legendre(t, x, state=state):
+            state[:] = np.dot(Ad, state) + np.dot(Bd, x[None, :])
+            return state.T.flatten()
+        return step_legendre
+
+    def get_weights_for_delays(self, r):
+        # compute the weights needed to extract the value at time r
+        # from the network (r=0 is right now, r=1 is theta seconds ago)
+        r = np.asarray(r)
+        m = np.asarray([legendre(i)(2*r - 1) for i in range(self.q)])
+        return m.reshape(self.q, -1).T
+#ldn = nengo.Node(LDN(theta=theta, q=q))   
+
+
+class PredictiveModel_LMU:
+    def __init__(self, seed=42, neurons_per_dim=100, sample_freq=50, lmu_theta=1, lmu_q=10,
+                 t_delay=0.02, learning_rate=5e-5, action_vars=["Q"],
+                 state_vars=["angle_sin", "angleD", "position", "positionD"]):
+        self.seed = seed
+        self.neurons_per_dim = neurons_per_dim
+        self.sample_freq = sample_freq
+        self.lmu_q=lmu_q
+        self.lmu_theta=lmu_theta 
+        self.t_delay = t_delay
+        self.learning_rate = learning_rate
+        self.action_vars = action_vars
+        self.action_dim = len(action_vars)
+        self.state_vars = state_vars
+        self.state_dim = len(state_vars)
+
+    def make_model(self, action_df, state_df, weights=None):
+
+        for v in self.action_vars:
+            assert v in action_df.columns
+        for v in self.state_vars:
+            assert v in state_df.columns
+
+        if weights is None:
+            weights = np.zeros((
+                self.state_dim,
+                self.neurons_per_dim * (self.state_dim + self.action_dim))
+            )
+
+        model = nengo.Network()
+        with model:
+            # set the default synapse to None (normal default is 0.005s)
+            model.config[nengo.Connection].synapse = None
+
+            # the input to the network
+            def action_stim_func(t):
+                # I have no idea if this works...
+                r = [action_df[x].iloc[int(t * self.sample_freq)] for x in self.action_vars]
+                return tuple(r)
+
+            a = nengo.Node(action_stim_func)
+
+            # this function streams the state signal from file to node
+            def state_stim_func(t):
+                r = [state_df[x].iloc[int(t * self.sample_freq)] for x in self.state_vars]
+                return tuple(r)
+
+            s = nengo.Node(state_stim_func)
+
+            # the value to be predicted (which in this case is just the first dimension of the input)
+            z = nengo.Node(None, size_in=self.state_dim)
+            nengo.Connection(s, z)
+
+            z_pred = nengo.Node(None, size_in=self.state_dim)
+
+
+            # make the LDN layer
+
+            ldn1 = nengo.Node(LDN(theta=self.lmu_theta, q=self.lmu_q, size_in=self.state_dim + self.action_dim))
+            nengo.Connection(a, ldn1[:self.action_dim])
+            nengo.Connection(s, ldn1[self.action_dim:])
+
+            # make the hidden layer
+            ens = nengo.Ensemble(
+                n_neurons=self.neurons_per_dim * (self.state_dim + self.action_dim),
+                dimensions=(self.state_dim + self.action_dim),
+                neuron_type=nengo.LIFRate(),
+                seed=self.seed
+            )
+
+            nengo.Connection(ldn1, ens)
+
+            # nengo.Connection(a, ens[:self.action_dim])
+            # nengo.Connection(s, ens[self.action_dim:])
+
+            # make the output weights we can learn
+            conn = nengo.Connection(ens.neurons, z_pred,
+                                    transform=weights,  # change this if you have pre-recorded weights to use
+                                    learning_rule_type=nengo.PES(learning_rate=self.learning_rate,
+                                                                 pre_synapse=DiscreteDelay(self.t_delay)
+                                                                 # delay the activity value when updating weights
+                                                                 ))
+
+            # compute the error by subtracting the current measurement from a delayed version of the predicton
+            error = nengo.Node(None, size_in=self.state_dim)
+            nengo.Connection(z_pred, error, synapse=DiscreteDelay(self.t_delay))
+            nengo.Connection(z, error, transform=-1)
+            # apply the error to the learning rule
+            nengo.Connection(error, conn.learning_rule)
+
+            # record the input to the network
+            p_a = nengo.Probe(a)
+            p_s = nengo.Probe(s)
+            # record the value to be predicted
+            p_z = nengo.Probe(z)
+            # record the prediction
+            p_z_pred = nengo.Probe(z_pred)
+            # record the error
+            p_e = nengo.Probe(error)
+            # record the weights (but only every 0.1 seconds just to save memory)
+            p_weights = nengo.Probe(conn, 'weights', sample_every=0.1)
+
+        return model, [p_a, p_s, p_z, p_z_pred, p_e, p_weights]
+
+
