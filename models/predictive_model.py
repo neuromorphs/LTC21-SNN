ed3042a8cf1256456991e7c220723b8f96eb1b1a
Justus Hübotter <justus.huebotter@donders.ru.nl>
2021-07-06 19:58:02 +0200

    adding script to test trained model on validation data

--- models/predictive_model.py
+++ models/predictive_model.py
@@ -29,6 +29,99 @@ class DiscreteDelay(nengo.synapses.Synapse):
 
         return step_delay
 
+class PredictiveModel:
+    def __init__(self, seed=42, neurons_per_dim=100, sample_freq=50,
+                 t_delay=0.02, learning_rate=5e-5, action_vars=["Q"],
+                 state_vars=["angle_sin", "angleD", "position", "positionD"]):
+        self.seed = seed
+        self.neurons_per_dim = neurons_per_dim
+        self.sample_freq = sample_freq
+        self.t_delay = t_delay
+        self.learning_rate = learning_rate
+        self.action_vars = action_vars
+        self.action_dim = len(action_vars)
+        self.state_vars = state_vars
+        self.state_dim = len(state_vars)
+
+    def make_model(self, action_df, state_df, weights=None):
+
+        for v in self.action_vars:
+            assert v in action_df.columns
+        for v in self.state_vars:
+            assert v in state_df.columns
+
+        if weights is None:
+            weights = np.zeros((
+                self.state_dim,
+                self.neurons_per_dim * (self.state_dim + self.action_dim))
+            )
+
+        model = nengo.Network()
+        with model:
+            # set the default synapse to None (normal default is 0.005s)
+            model.config[nengo.Connection].synapse = None
+
+            # the input to the network
+            def action_stim_func(t):
+                # I have no idea if this works...
+                r = [action_df[x].iloc[int(t * self.sample_freq)] for x in self.action_vars]
+                return tuple(r)
+
+            a = nengo.Node(action_stim_func)
+
+            # this function streams the state signal from file to node
+            def state_stim_func(t):
+                r = [state_df[x].iloc[int(t * self.sample_freq)] for x in self.state_vars]
+                return tuple(r)
+
+            s = nengo.Node(state_stim_func)
+
+            # the value to be predicted (which in this case is just the first dimension of the input)
+            z = nengo.Node(None, size_in=self.state_dim)
+            nengo.Connection(s, z)
+
+            z_pred = nengo.Node(None, size_in=self.state_dim)
+
+            # make the hidden layer
+            ens = nengo.Ensemble(
+                n_neurons=self.neurons_per_dim * (self.state_dim + self.action_dim),
+                dimensions=(self.state_dim + self.action_dim),
+                neuron_type=nengo.LIFRate(),
+                seed=self.seed
+            )
+            nengo.Connection(a, ens[:self.action_dim])
+            nengo.Connection(s, ens[self.action_dim:])
+
+            # make the output weights we can learn
+            conn = nengo.Connection(ens.neurons, z_pred,
+                                    transform=weights,  # change this if you have pre-recorded weights to use
+                                    learning_rule_type=nengo.PES(learning_rate=self.learning_rate,
+                                                                 pre_synapse=DiscreteDelay(self.t_delay)
+                                                                 # delay the activity value when updating weights
+                                                                 ))
+
+            # compute the error by subtracting the current measurement from a delayed version of the predicton
+            error = nengo.Node(None, size_in=self.state_dim)
+            nengo.Connection(z_pred, error, synapse=DiscreteDelay(self.t_delay))
+            nengo.Connection(z, error, transform=-1)
+            # apply the error to the learning rule
+            nengo.Connection(error, conn.learning_rule)
+
+            # record the input to the network
+            p_a = nengo.Probe(a)
+            p_s = nengo.Probe(s)
+            # record the value to be predicted
+            p_z = nengo.Probe(z)
+            # record the prediction
+            p_z_pred = nengo.Probe(z_pred)
+            # record the error
+            p_e = nengo.Probe(error)
+            # record the weights (but only every 0.1 seconds just to save memory)
+            p_weights = nengo.Probe(conn, 'weights', sample_every=0.1)
+
+        return model, [p_a, p_s, p_z, p_z_pred, p_e, p_weights]
+
+
 # move this to a model file
 def make_model(action_df, state_df, weights=None, seed=42, n=100, samp_freq=50,
                t_delay=0.02, learning_rate=5e-5):
---
+++ testing_prediction.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+import nengo
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use("Qt5Agg")
+import os
+from tqdm import tqdm
+from pathlib import Path
+import datetime
+from models.predictive_model import make_model
+
+experiment_name = "test1"
+data_dir = "data/Validate/"
+results_dir = "results/"
+load_weights = "results/test1/2021-07-06_17.48.05.498405/weights_latest.npy"
+
+assert Path(load_weights).is_file()
+
+bound = 0.19            # if the cart ever leaves these bounds, the data is ignored
+
+# TODO These parameters should be loaded from some model state_dict!
+samp_freq = 50          # cartpole data is recorded at ~50Hz
+dt = 0.001              # nengo time step
+learning_rate = 0       # lr
+t_delay = 0.02          # how far to predict the future (initial guess)
+neurons_per_dim = 100   # number of neurons representing each dimension
+seed = 4                # to get reproducible neuron properties across runs
+
+# crating a unique folder to save the weights in
+folder_name = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
+run_dir = Path(results_dir, experiment_name, folder_name)
+run_dir.mkdir(parents=True, exist_ok=True)
+
+P_A = 0
+P_S = 1
+P_Z = 2
+P_Z_PRED = 3
+P_E = 4
+P_WEIGHTS = 5
+
+# load training data from disk
+test_data = []
+for _, _, files in os.walk(data_dir):
+    for f in files:
+        df = pd.read_csv(data_dir + f, skiprows=28)
+        # Filter datasets that might have bounced of the edges of the track
+        if df["position"].min() < -bound or df["position"].max() > bound:
+            continue
+        test_data.append(df)
+    print(f"test data contains {len(test_data)} files")
+
+# init weights from file or empty
+print("loading weights from", load_weights)
+weights = np.load(load_weights)
+
+# test the model
+all_errors = []
+all_baseline_errors = []
+print("\nstarting model evaluation")
+epoch_mean_errors = []
+epoch_baseline_errors = []
+with tqdm(total=len(test_data)) as t:
+    for i, df in enumerate(test_data):
+        action_df = df[["time", "Q"]]
+        state_df = df[["time",
+                       # "angle",
+                       "angleD",
+                       # "angleDD",
+                       # "angle_cos",
+                       "angle_sin",
+                       "position",
+                       "positionD",
+                       # "positionDD",
+                       # "target_position",
+                       ]]
+        t_max = action_df["time"].max()   # number of seconds to run
+        # TODO use the OO method of creating the model (see predictive_model.py)
+        model, recordings = make_model(
+            action_df,
+            state_df,
+            weights=weights,
+            seed=seed,
+            n=neurons_per_dim,
+            samp_freq=samp_freq,
+            t_delay=t_delay,
+            learning_rate=learning_rate
+        )
+        sim = nengo.Simulator(model, progress_bar=False)
+        sim.run(t_max)
+        #weights = sim.data[recordings[P_WEIGHTS]][-1]
+        p_e = sim.data[recordings[P_E]]
+        p_z = sim.data[recordings[P_Z]]
+        p_z_pred = sim.data[recordings[P_Z_PRED]]
+        p_s = sim.data[recordings[P_S]]
+
+        mean_error = np.mean(np.abs(p_e))
+        baseline_error = np.mean(np.abs(p_z_pred - p_s))
+        epoch_mean_errors.append(mean_error)
+        epoch_baseline_errors.append(baseline_error)
+        all_errors.append(mean_error)
+        all_baseline_errors.append(baseline_error)
+        t.set_postfix(loss="{:05.4f}".format(mean_error))
+        t.update()
+
+print(f"\nepoch mean loss: {np.mean(epoch_mean_errors)}")
+
+plt.plot(range(len(all_errors)), all_errors)
+plt.plot(range(len(all_baseline_errors)), all_baseline_errors)
+plt.xlabel("Example")
+plt.ylabel("Error")
+plt.legend(["Prediction", "Baseline"])
+plt.savefig(Path(run_dir, "error_curve_test.svg"))
+# plt.show()
+plt.close()
